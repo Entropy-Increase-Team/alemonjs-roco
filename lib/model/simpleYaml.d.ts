012c1db189c654bc545aaeb22e4837cb80d553dd
ningmengchongshui <ningmengchongshui@gmail.com>
2026-05-15 20:06:06 +0800

    v0.0.10

--- lib/model/simpleYaml.d.ts
+++ lib/model/simpleYaml.d.ts
@@ -1,4 +1,5 @@
 export declare function parseYamlText<T = unknown>(content: string): T;
 export declare function parseSimpleYaml(filePath: string): Record<string, unknown>;
 export declare function parseYamlResource<T = unknown>(resource: URL): T;
+export declare function parseImportedYamlResource<T = unknown>(resource: unknown): T;
 export declare function deepMerge<T extends Record<string, unknown>>(base: T, override: Record<string, unknown>): T;
